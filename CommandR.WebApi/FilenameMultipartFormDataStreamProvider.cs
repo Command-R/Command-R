edf872aa8a1048cbd0647cf785928e1a884bc3ed
Paul Wheeler <paul@paulwheeler.com>
2015-07-12 19:44:09 -0400

    add support for rpc file uploads

---
+++ CommandR.WebApi/FilenameMultipartFormDataStreamProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace CommandR.WebApi
+{
+    //REF: http://bartwullems.blogspot.com/2013/03/web-api-file-upload-set-filename.html
+    public class FilenameMultipartFormDataStreamProvider : MultipartFormDataStreamProvider
+    {
+        public FilenameMultipartFormDataStreamProvider(string path) : base(path)
+        {
+        }
+
+        public override string GetLocalFileName(HttpContentHeaders headers)
+        {
+            return string.Format("{0}_{1}",
+                Guid.NewGuid().ToString("N"),
+                Path.GetFileName(headers.ContentDisposition.FileName.Replace("\"", string.Empty)));
+        }
+    };
+}
--- CommandR.WebApi/JsonRpcController.cs
+++ CommandR.WebApi/JsonRpcController.cs
@@ -1,8 +1,14 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web.Hosting;
 using System.Web.Http;
+using CfgDotNet;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace CommandR.WebApi
 {
@@ -11,17 +17,19 @@ namespace CommandR.WebApi
     {
         private readonly JsonRpc _jsonRpc;
         private readonly Commander _commander;
+        private readonly Settings _settings;
 
-        public JsonRpcController(JsonRpc jsonRpc, Commander commander)
+        public JsonRpcController(JsonRpc jsonRpc, Commander commander, Settings settings)
         {
             _jsonRpc = jsonRpc;
             _commander = commander;
+            _settings = settings;
         }
 
         [Route("{command?}")]
         public async Task<dynamic> Post(string command = null)
         {
-            var json = await Request.Content.ReadAsStringAsync();
+            var json = await RetrieveJsonFromRequest(Request.Content);
             return await _jsonRpc.Execute(json);
         }
 
@@ -32,5 +40,71 @@ namespace CommandR.WebApi
             var json = JsonConvert.SerializeObject(queryString);
             return await _commander.Send(command, json);
         }
+
+        //REF: http://shazwazza.com/post/uploading-files-and-json-data-in-the-same-request-with-angular-js/
+        private async Task<string> RetrieveJsonFromRequest(HttpContent content)
+        {
+            if (!content.IsMimeMultipartContent())
+            {
+                return await content.ReadAsStringAsync();
+            }
+
+            var provider = LoadMultipartStreamProvider();
+            var result = await content.ReadAsMultipartAsync(provider);
+            return MapFilePathsToCommandJson(result);
+        }
+
+        private MultipartFormDataStreamProvider LoadMultipartStreamProvider()
+        {
+            var path = _settings.UploadFolder;
+            if (path == null)
+                throw new ApplicationException("JsonRpcController ERROR null uploads path");
+
+            if (path.StartsWith("~"))
+            {
+                path = HostingEnvironment.MapPath(path) ?? string.Empty;
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
+
+            return new FilenameMultipartFormDataStreamProvider(path);
+        }
+
+        private static string MapFilePathsToCommandJson(MultipartFormDataStreamProvider result)
+        {
+            var json = result.FormData["json"];
+            if (json == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var jObject = JObject.Parse(json);
+            foreach (var file in result.FileData)
+            {
+                var name = file.Headers.ContentDisposition.Name.Replace("\"", string.Empty);
+                jObject["params"][name] = file.LocalFileName;
+            }
+
+            return jObject.ToString();
+        }
+
+        public class Settings : BaseSettings
+        {
+            public string UploadFolder { get; set; }
+
+            public override void Validate()
+            {
+                if (string.IsNullOrWhiteSpace(UploadFolder))
+                    UploadFolder = "~/App_Data/JsonRpcUploads/";
+
+                if (UploadFolder.StartsWith("~"))
+                    return; //allow mapped paths, but can't fail fast
+
+                if (!Directory.Exists(UploadFolder))
+                    Directory.CreateDirectory(UploadFolder);
+            }
+        };
     };
 }
